db7e40300f0b93fd5777e72589c3e2d60b033f99
celsius narhwal <hello@celsiusnarhwal.dev>
2023-06-12 14:32:06 -0400

    update plugin.py

--- artwall/plugin.py
+++ artwall/plugin.py
@@ -1,11 +1,20 @@
 from typing import Optional
 
+from dict_deep import deep_get, deep_set
 from mkdocs.config.base import Config
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import BasePlugin
+from path import Path
+
+icon_dir = Path(__file__).parent / ".overrides" / ".icons"
 
 
 class ArtwallPlugin(BasePlugin):
     def on_config(self, config: MkDocsConfig) -> Optional[Config]:
-        config.theme.dirs.append(".overrides")
+        config.theme.dirs.insert(1, icon_dir.parent)
+
+        key = "mdx_configs|pymdownx.emoji|options|custom_icons"
+        custom_icons = (deep_get(config, key, sep="|") or []) + [str(icon_dir)]
+        deep_set(config, key, custom_icons, sep="|")
+
         return config
